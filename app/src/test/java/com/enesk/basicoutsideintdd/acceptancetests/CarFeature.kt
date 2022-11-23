f86c36bbce95d74f58acd594c0102a872a664bf9
kocerenes <eneskocerr41@gmail.com>
2022-11-23 12:56:35 +0300

    Failing acceptance test.

--- app/src/test/java/com/enesk/basicoutsideintdd/acceptancetests/CarFeature.kt
+++ app/src/test/java/com/enesk/basicoutsideintdd/acceptancetests/CarFeature.kt
@@ -2,15 +2,25 @@ package com.enesk.basicoutsideintdd.acceptancetests
 
 import com.enesk.basicoutsideintdd.Car
 import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertTrue
 import org.junit.Test
 
 class CarFeature {
 
-    private val car = Car(6.0)
+    private val engine = Engine()
+    private val car = Car(6.0, engine)
 
     @Test
     fun carIsLoosingFuelWhenItTurnOn() {
         car.turnOn()
         assertEquals(5.5, car.fuel)
+    }
+
+    @Test
+    fun carIsTurningOnItsEngineAndIncreasesTheTemperature() {
+        car.turnOn()
+
+        assertEquals(95, car.engine.temperature)
+        assertTrue(car.engine.isTurnedOn)
     }
 }
