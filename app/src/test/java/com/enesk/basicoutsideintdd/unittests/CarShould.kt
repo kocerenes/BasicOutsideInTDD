5709956d6c4d2b534596b1f8f08860a2a9496010
kocerenes <eneskocerr41@gmail.com>
2022-11-23 13:35:14 +0300

    Create the red point.

--- app/src/test/java/com/enesk/basicoutsideintdd/unittests/CarShould.kt
+++ app/src/test/java/com/enesk/basicoutsideintdd/unittests/CarShould.kt
@@ -3,9 +3,13 @@ package com.enesk.basicoutsideintdd.unittests
 import com.enesk.basicoutsideintdd.Car
 import junit.framework.TestCase.assertEquals
 import org.junit.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.times
+import org.mockito.kotlin.verify
 
 class CarShould {
 
+    private val engine: Engine = mock()
     private val car = Car(5.0)
 
     @Test
@@ -13,5 +17,11 @@ class CarShould {
         car.turnOn()
 
         assertEquals(4.5, car.fuel)
+    }
+
+    @Test
+    fun turnOnItsEngine() {
+        car.turnOn()
+        verify(engine, times(1)).turnOn()
     }
 }
